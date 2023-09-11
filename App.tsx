feb3d6d5ba6dc73c9f0341eb932b8d64e52b1ac5
CyrilDehaese <66725473+CyrilDehaese@users.noreply.github.com>
2023-09-11 17:40:17 +0200

    Init

--- App.tsx
+++ App.tsx
@@ -1,29 +1,11 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
 
-import RegisterPage from './src/Pages/RegisterPage';
-import LoginPage from './src/Pages/LoginPage';
-import AdminLoginPage from './src/Pages/AdminLoginPage';
-import CguPage from './src/Pages/CguPage';
-import HomePage from './src/Pages/HomePage';
-import TestPage from './src/Pages/TestPage';
 
-const Stack = createStackNavigator();
-
-function App() {
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Test" component={TestPage} options={{ headerShown: false }} />
-        <Stack.Screen name="Register" component={RegisterPage} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={LoginPage} options={{ headerShown: false }} />
-        <Stack.Screen name="AdminLogin" component={AdminLoginPage} options={{ headerShown: false }} />
-        <Stack.Screen name="Cgu" component={CguPage} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={HomePage} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <WebView
+      source={{ uri: 'https://expo.dev' }}
+    />
   );
 }
-
-export default App;
--- src/NewComponents/DirectionPopup.style.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-import styleReference from './Style';
-
-const styles = StyleSheet.create({
-    
-});
-
-export default styles;
